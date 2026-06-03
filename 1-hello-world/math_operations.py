53dfe3d2a4e6036ec530d5cdef801128fd62671b
Benjamin Avila <badjr23@hotmail.com>
2026-06-03 16:30:00 -0600

    15-operaciones-aritmeticas

---
+++ 1-hello-world/math_operations.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3.5
+
+add = a + b
+substract = a - b
+division = a / b
+product = a * b
+
+integer_division = a // b #cuando tienes doble slash es una division entera
+module = a % b
+pow = a ** b
+
+print(f'Suma: {add}')
+print(f'Resta: {substract}')
+print(f'multiplicacion: {product}')
+print(f'Division: {division}')
+print(f'Division entera: {integer_division}')
+print(f'Modulo: {module}')
+print(f'Potencia: {pow}')
