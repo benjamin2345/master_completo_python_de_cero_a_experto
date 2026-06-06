537af3a56f9ef55da65cbb2516aef41e06f45617
Benjamin Avila <badjr23@hotmail.com>
2026-06-06 13:07:00 -0600

    18-sentencia-for

---
+++ 1-hello-world/for-statements.py
@@ -0,0 +1,23 @@
+
+for elemento in range(5):
+    print(elemento)
+
+print('===============  inicia en 2 e incrementa en 2')
+
+for i in range(0, 10 , 2):
+    print(i)
+
+print('=============== decrementa')
+
+for i in range(10, 0 , -1):
+    print(i)
+
+print('===============')
+for i in range(10, -1, -1):
+    print(i)
+
+print('===============')
+names = ['Andres', 'Pepe', 'John', 'Juan', 1000, 3.1415]
+for name in names:
+    print(name)
+
