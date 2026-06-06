490370b5a3d7f0fd6b6e8343511ab1768637bc58
Benjamin Avila <badjr23@hotmail.com>
2026-06-06 12:49:31 -0600

    17-sentencia-if

---
+++ 1-hello-world/if-statements.py
@@ -0,0 +1,25 @@
+age = 25
+
+if age == 25:
+    print('correcto tienes 25')
+
+if age >= 18:
+    print('Eres mayor de edad')
+
+age = 15
+
+if age >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+nota = 8.9
+
+if nota >= 9:
+    print("Excelemte")
+elif nota >= 7.5:
+    print("Muy bien")
+elif nota >= 5:
+    print("Bien aprovado!")
+else:
+    print("Reprobado")
