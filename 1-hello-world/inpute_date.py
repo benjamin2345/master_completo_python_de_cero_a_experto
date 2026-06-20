9a1b2ff4981de0cbef47ece342b21dbea6b2113c
Benjamin Avila <badjr23@hotmail.com>
2026-06-20 11:10:04 -0600

    25-entrada-de-datos

---
+++ 1-hello-world/inpute_date.py
@@ -0,0 +1,16 @@
+def main():
+    name = input('introduce el nombre del producto: ')
+    print(name)
+
+    price = int(input('introduce el precio del producto en dolares: '))
+    print(f'El valor final {price} pesos:')
+
+    weight = float(input('Ingresa el peso en gramos: '))
+    print(f'Pesa {weight} gramos.')
+
+try:
+    main()
+
+except ValueError:
+    print('Error: debes introducir bien los datos, decimal es con punto!')
+    main()
